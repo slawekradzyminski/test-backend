d18776824bc44c2ab85da057fe9743fa19b7fbcd
Slawek <s.radzyminski@ocado.com>
2021-03-20 14:29:43 +0100

    Added requests logging

--- src/main/java/com/awesome/testing/SampleApi.java
+++ src/main/java/com/awesome/testing/SampleApi.java
@@ -3,6 +3,7 @@ package com.awesome.testing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -20,6 +21,16 @@ public class SampleApi {
                 .apis(RequestHandlerSelectors.basePackage("com.awesome.testing.controller"))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
     }
 
     public static void main(String[] args) {
